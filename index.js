0ed31c6923ff6b8b3bb727d3fc27af38c8a272df
MeowMerry <meow3525@gmail.com>
2023-02-17 09:43:39 -0600

    Final SSR with Firebase and React

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ import {renderToString} from 'react-dom/server';
 import App from './src/App';
 import getFacts from './src/facts';
 import express from 'express';
+import fs from 'fs';
+
+const index = fs.readFileSync(__dirname + '/index.html', 'utf8')
 
 const app = express();
 app.get('**', (req,res)=>{
     getFacts().then(facts=>{
         const html = renderToString(<App facts={facts}/>);
+        const finalHtml = index.replace('<!--------:: APP:: ------->',html)
         res.set('Cache-Control', 'public, max-age=600, s-maxage=1200');
         res.send(html)
     });
